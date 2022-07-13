28418314cf65e1bc4596246ad1d5a1ab6d714028
juan gamarra <asd@asd.com>
2022-07-13 20:30:41 -0300

    Comienzo del proyecto

---
+++ pruebasgit/Ejecutar.java
@@ -0,0 +1,13 @@
+package pruebasgit;
+
+public class Ejecutar {
+
+    public static void main(String[] args) {
+
+        Saludo saludo = new Saludo();
+
+        System.out.println(saludo.getSaludo());
+
+    }
+
+}
---
+++ pruebasgit/Saludo.java
@@ -0,0 +1,22 @@
+package pruebasgit;
+
+public class Saludo {
+
+    private String saludoBienvenida;
+    
+    public Saludo() {
+        
+        saludoBienvenida = "hola mundo";
+    }
+
+    public String getSaludo() {
+        return saludoBienvenida;
+    }
+
+    public void setSaludo(String saludo) {
+        this.saludoBienvenida = saludo;
+    }
+    
+    
+    
+}
